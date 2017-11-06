7b00a84902dc2a6c04c653aaaa114a03664562d5
pylipp <philipp.metzner@tum.de>
2017-11-06 13:24:39 +0100

    Deprecate srv/GetInput.srv.

--- catkin_ws/src/dartbox/test/service.py
+++ catkin_ws/src/dartbox/test/service.py
@@ -1,11 +1,14 @@
 #! /usr/bin/env python
 
-from dartbox.srv import GetInput, GetInputResponse
+"""Server running on the Raspi instead of the arduino."""
+
+from rosserial_arduino.srv import Test as GetInput
+from rosserial_arduino.srv import TestResponse as GetInputResponse
 import rospy
 
 
 def handle_request(request):
-    return GetInputResponse(raw_input(request.prompt))
+    return GetInputResponse(raw_input(request.input))
 
 if __name__ == "__main__":
     rospy.init_node("get_input_server")
