ace26f770caf1653382bbd06387121a4857e7b8e
pylipp <philipp.metzner@tum.de>
2017-11-10 01:26:26 +0100

    Overwrite print_output() in RosCommunicator.

--- catkin_ws/src/dartbox/src/dartbox/communication.py
+++ catkin_ws/src/dartbox/src/dartbox/communication.py
@@ -1,7 +1,8 @@
 import rospy
 
 from pydarts.communication import (CommunicatorBase, SanitizationError,
-        sanitized_input, MinLargerMaxError,
+        sanitized_input, MinLargerMaxError, ERROR, INFO_VISIT, INFO_FINISH,
+        INFO_LEG, finishes
         )
 
 
@@ -22,9 +23,30 @@ class RosCommunicator(CommunicatorBase):
                     sanitized_input_))
                 return sanitized_input_
             except SanitizationError as e:
-                self.print_output(str(e))
+                self.print_output(ERROR, error=e)
 
                 if isinstance(e, MinLargerMaxError):
                     # re-raise to avoid infinite loop
                     raise
 
+    def print_output(self, message_type, **data):
+        output = None
+
+        if message_type == ERROR:
+            output = str(data["error"])
+        elif message_type == INFO_VISIT:
+            player = data["player"]
+            output = "{p.name} - {p.score_left} - {0}".format(
+                    player.darts * "|", p=player)
+        elif message_type == INFO_FINISH:
+            player = data["player"]
+            if player.score_left in finishes:
+                output = " ".join(finishes[player.score_left])
+        elif message_type == INFO_LEG:
+            players = data["players"]
+            output = ", ".join(
+                    ("{}: {:1d}".format(p.name, p.nr_won_legs) for p in players)
+                    )
+
+        if output is not None:
+            self._output_method(output)
