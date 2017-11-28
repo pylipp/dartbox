f0ab0f210f829091c3533d3e0296c6a5e29740a9
pylipp <philipp.metzner@tum.de>
2017-11-28 01:10:51 +0100

    Implement RosCommunicator.get_input().

--- catkin_ws/src/dartbox/src/dartbox/communication.py
+++ catkin_ws/src/dartbox/src/dartbox/communication.py
@@ -1,11 +1,40 @@
+import time
+
 from pydarts.communication import (CommunicatorBase, INFO_VISIT, INFO_FINISH,
-                                   INFO_LEG, finishes)
+                                   INFO_LEG, finishes, INPUT_ANOTHER_SESSION)
 
 
 class RosCommunicator(CommunicatorBase):
     """Subclass providing communication using ROS via the '/get_input' service
     and the 'print_info'/'print_error' topics.
     """
+
+    def __init__(self, input_method, output_info_method,
+                 output_error_method=None):
+        super(RosCommunicator, self).__init__(
+                input_method, output_info_method, output_error_method)
+        self._input_prompts[INPUT_ANOTHER_SESSION] = {
+            "prompt": "Again? ",
+            "kwargs": {"choices": "ABC"},  # our keypad keys...
+        }
+
+    def get_input(self, input_mode, *format_args):
+        """Custom implementation mapping the keypad options to the y/n/q choices
+        when querying for another session.
+        """
+
+        if input_mode == INPUT_ANOTHER_SESSION:
+            self._output_info_method("%A=yes, B=no, C=quit")
+            # minimal delay, otherwise service request gets lost
+            time.sleep(0.1)
+
+        user_input = super(RosCommunicator, self).get_input(input_mode,
+                *format_args)
+
+        if input_mode == INPUT_ANOTHER_SESSION:
+            user_input = {"A": "y", "B": "n", "C": "q"}[user_input]
+
+        return user_input
 
     def print_info(self, message_type, **data):
         output = None
